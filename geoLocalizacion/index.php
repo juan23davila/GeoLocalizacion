3d0edbe081084f5dd51f273594c52407feb97260
Juan Davila <juan23davila@gmail.com>
2013-09-01 14:18:17 -0500

    Revert "Simplemente cree un nuevo div sin importancia"
    This reverts commit b2e3a607daa37f5d3ae3501e4e1e00faed01a57f.

--- geoLocalizacion/index.php
+++ geoLocalizacion/index.php
@@ -11,9 +11,6 @@
         // put your code here
         ?>
         <div id="fondo">
-            <div id="peubaGit">
-                
-            </div>
             
             <div id="global">
                 <h2>Pagina para localizar un país de un servidor</h2>
