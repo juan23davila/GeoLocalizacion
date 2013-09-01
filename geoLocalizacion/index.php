b663a2b0a41431ef45010b3ea6ede55fdc3f4940
Juan <Juan@Juan-PC>
2013-09-01 11:41:45 -0500

    Creando proyecto

---
+++ geoLocalizacion/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>GeoIp</title>
+        <link href="estilo.css" media="screen" rel="StyleSheet" type="text/css">
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <div id="fondo">
+            
+            <div id="global">
+                <h2>Pagina para localizar un país de un servidor</h2>
+                
+                <div id="t">
+                <form action="http://www.webservicex.net/geoipservice.asmx/GetGeoIP" method="get" enctype="multipart/form-data">
+                    <table border="0" cellpadding="0" cellspacing="0" id="tabla">
+                        <tr>
+                            <td>
+                                Localizar la siguiente IP:
+                            </td>
+                            <td>
+                                
+                            </td>
+                        </tr>
+                    
+                        <tr>
+                            <td>
+                                <input name="IPAddress" type="text" maxlength="50">
+                            </td>
+                            <td>
+                                <input name="Enviar" type="submit" value="Enviar">
+                            </td>
+                        </tr>
+                    </table> 
+                </form>
+                </div>
+                
+                <!--Aqui crearé un formulario para la palicación-->
+                <br>
+           <!--     <form name="formulario" enctype="multipart/form-data" method="post" action="proceso.php">
+                    <input name="ip" type="text"/>
+                    <input name="enviar" type="submit" value="Enviar" />
+                </form>
+            </div>-->
+            
+        </div>
+    </body>
+</html>
