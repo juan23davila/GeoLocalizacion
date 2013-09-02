5da33aa391da5538b56473fdd4cb197e745e7a4c
Juan <Juan@Juan-PC>
2013-09-02 16:35:00 -0500

    Termine, se hizo lo que se pudo

--- geoLocalizacion/mapa.php
+++ geoLocalizacion/mapa.php
@@ -10,10 +10,25 @@
     <script type="text/javascript"
       src="http://maps.googleapis.com/maps/api/js?key=AIzaSyC6LUtFPZGSxd_7002hqKtSPIBMDyM4W0U&sensor=false">
     </script>
-    <script type="text/javascript">
+  </head>
+  <body onload="initialize()">
+        
+      <script type="text/javascript">
+        <?php
+            $array = array($_POST[10],$_POST[11]);
+        ?>
+            var jsarray   = [];
+         <?php
+            for($i=0; $i < count($array); $i++ ){
+                echo 'jsarray['.$i.'] = "'.$array[$i].'";';
+            }
+         ?>
+         alert(jsarray[0]);   
+         alert(jsarray[1]);
+            
       function initialize() {
         var mapOptions = {
-          center: new google.maps.LatLng(4.5339, -75.6811 ),
+          center: new google.maps.LatLng(4.5339 , -75.6811),
           zoom: 15,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
@@ -21,8 +36,6 @@
             mapOptions);
       }
     </script>
-  </head>
-  <body onload="initialize()">
     <div id="map_canvas" style="width:100%; height:100%"></div>
   </body>
 </html>
