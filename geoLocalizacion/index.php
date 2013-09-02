72660122a864c107d6168594205dd4331f903a48
Juan <Juan@Juan-PC>
2013-09-02 14:35:08 -0500

    Hasta aqui ya imprime latitud y longitud

--- geoLocalizacion/index.php
+++ geoLocalizacion/index.php
@@ -49,15 +49,22 @@
                     //Ahora se realiza la respectiva peticion al webservice
                     curl_setopt($ch, CURLOPT_URL, $url);
                     $respuesta = curl_exec($ch);
+                    echo $respuesta."<br>";
                         
-                    //Ahora se analiza el XML obtenido para saber si puede ser usado
+                    //Este codigo es el que quisiera que sirviera
                     $freegeoip = simplexml_load_string($respuesta);
                     foreach ($freegeoip->Resultado as $resultado){
-                    $latitud = $resultado->Latitude;
-                    $longitud = $resultado->Longitud;
+                        $latitud = $resultado->Latitude;
+                        $longitud = $resultado->Longitud;
                     }
+                    echo $latitud." ".$longitud;
                     
-                    
+                    //Este es el codigo chambon
+                    $array = explode(" ", $respuesta);
+                    echo "La latitud es: ".$array[10]."<br>";
+                    echo "La longitud es: ".$array[11]; 
+
+                    curl_close($ch);
                     ?>
                 </div>              
             </div>
