3cf543af7b141d8ea1f9eda4644f57f15ffd6d61
Juan <Juan@Juan-PC>
2013-09-05 18:53:39 -0500

    Se elimino codigo que sobraba de servicioWeb.php (que estaba en comentario o que no era de utilidad)

--- geoLocalizacion/servicioWeb.php
+++ geoLocalizacion/servicioWeb.php
@@ -6,12 +6,7 @@
        curl_setopt($ch, CURLOPT_HEADER, 0);
        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
        
-       $url = "http://freegeoip.net/xml/".$ip;
        $urlJson = "http://freegeoip.net/json/".$ip;
-
-       //Ahora se realiza la rehttp://freegeoip.net/json/spectiva peticion al webservice
-       curl_setopt($ch, CURLOPT_URL, $url);
-       $respuesta = curl_exec($ch);
        
        //Ahora se realiza la respectiva peticion al webservice en formato json
        curl_setopt($ch, CURLOPT_URL, $urlJson);
@@ -19,31 +14,11 @@
 
        //Este codigo es el que quisiera que sirviera
        $datos = json_decode($respuestaJson,true);
-       echo "La latitud es: ".$datos['latitude'];
-       echo "<br>";
-       echo "La longitud es: ".$datos['longitude'];
-       echo "<br>";
        $latitud = $datos['latitude'];
        $longitud = $datos['longitude'];
- //      $array = array($datos['latitude'],$datos['longitude']);
- //      enviarToMapa("mapa.php",$array);
        
        curl_close($ch);
        
-/*       function enviarToMapa($destino="",$dato=array()){
-	$r = false;
-	if($destino!="" ){
-		$ch = curl_init($destino);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt ($ch, CURLOPT_POST, 1);
-		curl_setopt ($ch, CURLOPT_POSTFIELDS, $dato);
-		$data = curl_exec ($ch);
-		curl_close ($ch);
-			if($data != "0")
-				$r = $data;
-	}
-	return $r;
-        }*/
 ?>
 <br>
 
