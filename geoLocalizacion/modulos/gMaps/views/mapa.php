7f1f029c7b05da53800c0209e5afba4a8f9e0d20
Juan <Juan@Juan-PC>
2013-09-12 19:35:50 -0500

    Se agrego el marcador del mapa

--- geoLocalizacion/modulos/gMaps/views/mapa.php
+++ geoLocalizacion/modulos/gMaps/views/mapa.php
@@ -1,20 +1,27 @@
 <!DOCTYPE html>
 
-        
-      <script type="text/javascript">
-       
-            var latitud = "<? echo $latitud;?>";
-            var longitud = "<? echo $longitud;?>";
-            
-      function initialize() {
+
+<script type="text/javascript">
+
+    var latitud = "<? echo $latitud; ?>";
+    var longitud = "<? echo $longitud; ?>";
+
+    function initialize() {
+        var miUbicacion = new google.maps.LatLng(latitud, longitud);
         var mapOptions = {
-          center: new google.maps.LatLng(latitud , longitud),
-          zoom: 15,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
+            center: miUbicacion,
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         var map = new google.maps.Map(document.getElementById("map_canvas"),
-            mapOptions);
-      }
-    </script>
-    <div id="map_canvas"></div>
+                mapOptions);
+
+        var marker = new google.maps.Marker({
+            position: miUbicacion,
+            map: map,
+            title: "Estas aqui!!"
+        });
+    }
+</script>
+<div id="map_canvas"></div>
 
--- geoLocalizacion/servicioWeb.php
+++
@@ -1,8 +0,0 @@
-<?php
-      
-       
-?>
-<br>
-
-<?php $variable="prueba"; ?>
-<a href="mapa.php?lat=<?php echo $latitud; ?>&lon=<?echo $longitud;?>"/>label</a>
